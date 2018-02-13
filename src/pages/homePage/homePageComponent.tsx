7ab2b8250649b019c37e1532c56ff6797dde68a8
fjcalzado <fjcalzadorodriguez@gmail.com>
2018-02-13 23:52:08 +0100

    Some minor styling for home page

--- src/pages/homePage/homePageComponent.tsx
+++ src/pages/homePage/homePageComponent.tsx
@@ -6,11 +6,14 @@ const styles = require("./homePageComponent.scss");
 export const HomePageComponent: React.StatelessComponent<{}> = () => {
   return (
     <div className={styles.container}>
-      <h2 className="jumbotron">React PoC</h2>
+      <h2 className={`jumbotron ${styles.header}`}>React PoC</h2>
       <br />
-      <Link to="/search">
-        <ButtonComponent>Search Page</ButtonComponent>
-      </Link>
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <Link to="/search">Search Page</Link>
+          {/* <a className="nav-link active" href="#">Active</a> */}
+        </li>
+      </ul>
     </div>
   )
 }
